495084cb560713cb09ba8ec388dd0027e0398fc5
Yuki Matsuo <yuki.matsuo@kinto-technologies.com>
2022-10-07 17:01:05 +0900

    Import extensions and errors

---
+++ langkawi-swiftui-ios/Extensions/ViewExtensions.swift
@@ -0,0 +1,23 @@
+//
+//  ViewExtensions.swift
+//  langkawi-swiftui-ios
+//
+//  Created by 松尾 勇気（Yuki Matsuo） on 2022/10/07.
+//
+
+import SwiftUI
+
+extension View {
+    
+    func showAlert(isPresented: Binding<Bool>, content: AlertContent?) -> some View {
+        alert(isPresented: isPresented) {
+            Alert(title: Text(content?.title ?? ""), message: Text(content?.message ?? ""))
+        }
+    }
+    
+    func showLogin(isPresented: Binding<Bool>) -> some View {
+        fullScreenCover(isPresented: isPresented) {
+            LoginRouter.assemble(isPresented: isPresented)
+        }
+    }
+}
