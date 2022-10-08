26db99c085d6b615c1c6d88f67e293ee4403341d
Yuki Matsuo <yuki.matsuo@kinto-technologies.com>
2022-10-08 14:09:47 +0900

    Update utils

--- langkawi-swiftui-ios/Utils/ColorDef.swift
+++ langkawi-swiftui-ios/Utils/ColorDef.swift
@@ -10,5 +10,7 @@ import SwiftUI
 class ColorDef {
     static let primary = Color.green
     static let secondary = Color.purple
+    static let background = Color.white
+    static let surface = Color(red: 245/255, green: 245/255, blue: 245/255)
     static let appBar = Color(red: 242/255, green: 242/255, blue: 242/255)
 }
