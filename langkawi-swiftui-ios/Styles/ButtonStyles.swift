01e0319cb81593386e15cf87d7e785f3aad8c6f5
Yuki Matsuo <yuki.matsuo@kinto-technologies.com>
2022-10-09 11:32:13 +0900

    Update style

--- langkawi-swiftui-ios/Styles/ButtonStyles.swift
+++ langkawi-swiftui-ios/Styles/ButtonStyles.swift
@@ -11,10 +11,10 @@ class ButtonStyles {
     
     static func Primary(_ title: String, action: @escaping () -> Void) -> some View {
         Button(title, action: action)
-        .font(Font.system(size: 20, weight: .bold))
-        .foregroundColor(.black)
-        .frame(width: 240, height: 50)
-        .background(ColorDef.primary)
-        .cornerRadius(25)
+            .font(Font.system(size: 20, weight: .bold))
+            .foregroundColor(.black)
+            .frame(width: 240, height: 50)
+            .background(ColorDef.primary)
+            .cornerRadius(25)
     }
 }
